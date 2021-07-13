1fc769625262b97adc76fa26e16e4876d67dfe31
Masa Cheung <63529499+masacheung@users.noreply.github.com>
2021-07-13 17:47:18 -0400

    Initial commit

---
+++ lib/problems.rb
@@ -0,0 +1,132 @@
+# Write a method, all_vowel_pairs, that takes in an array of words and returns all pairs of words
+# that contain every vowel. Vowels are the letters a, e, i, o, u. A pair should have its two words
+# in the same order as the original array. 
+#
+# Example:
+#
+# all_vowel_pairs(["goat", "action", "tear", "impromptu", "tired", "europe"])   # => ["action europe", "tear impromptu"]
+def all_vowel_pairs(words)
+    vowels = ['a','e','i','o','u']
+    ans = []
+    (0...words.length-1).each do |i|
+        (i+1..words.length-1).each do |j|
+            temp = words[i] + " " + words[j]
+            ans << temp if vowels.all? {|v| temp.include?(v)}
+        end
+    end
+    ans
+end
+
+
+# Write a method, composite?, that takes in a number and returns a boolean indicating if the number
+# has factors besides 1 and itself
+#
+# Example:
+#
+# composite?(9)     # => true
+# composite?(13)    # => false
+def composite?(num)
+    !is_prime?(num)
+end
+
+def is_prime?(num)
+    return false if num < 2
+    
+    (2...num).each do |i|
+        return false if num % i == 0
+    end
+    true
+end
+
+# A bigram is a string containing two letters.
+# Write a method, find_bigrams, that takes in a string and an array of bigrams.
+# The method should return an array containing all bigrams found in the string.
+# The found bigrams should be returned in the the order they appear in the original array.
+#
+# Examples:
+#
+# find_bigrams("the theater is empty", ["cy", "em", "ty", "ea", "oo"])  # => ["em", "ty", "ea"]
+# find_bigrams("to the moon and back", ["ck", "oo", "ha", "at"])        # => ["ck", "oo"]
+def find_bigrams(str, bigrams)
+    ans = []
+    bigrams.each do |big|
+        ans << big if str.include?(big)
+    end
+    ans
+end
+
+class Hash
+    # Write a method, Hash#my_select, that takes in an optional proc argument
+    # The method should return a new hash containing the key-value pairs that return
+    # true when passed into the proc.
+    # If no proc is given, then return a new hash containing the pairs where the key is equal to the value.
+    #
+    # Examples:
+    #
+    # hash_1 = {x: 7, y: 1, z: 8}
+    # hash_1.my_select { |k, v| v.odd? }          # => {x: 7, y: 1}
+    #
+    # hash_2 = {4=>4, 10=>11, 12=>3, 5=>6, 7=>8}
+    # hash_2.my_select { |k, v| k + 1 == v }      # => {10=>11, 5=>6, 7=>8})
+    # hash_2.my_select                            # => {4=>4}
+    def my_select(&prc)
+        prc ||= Proc.new {|k, v| k == v}
+        ans = Hash.new
+        self.each do |k, v|
+            ans[k] = v if prc.call(k, v)
+        end
+        ans
+    end
+end
+
+class String
+    # Write a method, String#substrings, that takes in a optional length argument
+    # The method should return an array of the substrings that have the given length.
+    # If no length is given, return all substrings.
+    #
+    # Examples:
+    #
+    # "cats".substrings     # => ["c", "ca", "cat", "cats", "a", "at", "ats", "t", "ts", "s"]
+    # "cats".substrings(2)  # => ["ca", "at", "ts"]
+    def substrings(length = nil)
+        ans = []
+        (0..self.length-1).each do |i|
+            ans << self[i]
+            (i+1...self.length).each do |j|
+                ans << self[i..j]
+            end
+        end
+        return ans if length == nil 
+        
+        lans = []
+        ans.each do |sub|
+            if sub.length == length
+                lans << sub
+            end
+        end
+        lans
+    end
+
+
+    # Write a method, String#caesar_cipher, that takes in an a number.
+    # The method should return a new string where each char of the original string is shifted
+    # the given number of times in the alphabet.
+    #
+    # Examples:
+    #
+    # "apple".caesar_cipher(1)    #=> "bqqmf"
+    # "bootcamp".caesar_cipher(2) #=> "dqqvecor"
+    # "zebra".caesar_cipher(4)    #=> "difve"
+    def caesar_cipher(num)
+        alp = ("a".."z").to_a
+        new_string = ""
+
+        self.each_char do |char|
+            temp = alp.index(char)
+            new_po = (temp + num) % 26
+            new_string += alp[new_po]
+        end
+        new_string
+    end
+end
+
